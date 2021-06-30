9c7b859ca9f77428c0bb4a7d2a39b9499a9ff791
Shadi Mahmoodian <shadi.mahm@gmail.com>
2021-06-30 21:29:59 +0430

    added client project

---
+++ src/Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TinCanPhone.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
