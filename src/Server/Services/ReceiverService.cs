8b5903ad9d2387de059d5253324bcb49e8cf2ba8
Shadi Mahmoodian <shadi.mahm@gmail.com>
2021-06-30 21:06:22 +0430

    separated proto from server project

--- src/Server/Services/ReceiverService.cs
+++ src/Server/Services/ReceiverService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
+using TinCanPhone.Protos;
 using TinCanPhone.Server.Handlers;
 
 namespace TinCanPhone.Server
