3a15693ffff54087d1660b54668a7dcff860b4a2
Shadi Mahmoodian <shadi.mahm@gmail.com>
2021-06-30 23:25:56 +0430

    added proto link to client project

--- src/Client/Client.cs
+++ src/Client/Client.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Grpc.Net.Client;
+using TinCanPhone.Protos;
 
 namespace TinCanPhone.Client
 {
     public class Client : IClient
     {
         private readonly GrpcChannel _channel;
+
+        private readonly Receiver.ReceiverClient _client;
 
         public Client(Uri serviceAddress)
         {
