27d26649e8c48481b0bf36323289c536d69a94bf
Shadi Mahmoodian <shadi.mahm@gmail.com>
2021-06-30 14:18:24 +0430

    added grpc server project

---
+++ src/Server/Services/ReceiverService.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace TinCanPhone.Server
+{
+    public class ReceiverService : Receiver.ReceiverBase
+    {
+        private readonly ILogger<ReceiverService> _logger;
+        public ReceiverService(ILogger<ReceiverService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<ResponseMessage> HandleMessages(RequestMessage request, ServerCallContext context)
+        {
+            return Task.FromResult(new ResponseMessage
+            {
+                Response = "Hello " + request.Message
+            });
+        }
+    }
+}
